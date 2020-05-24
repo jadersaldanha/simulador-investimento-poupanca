e47bb4760bf86e968574c3b6fe8851a3bdda848e
Jader de Freitas Saldanha <jader_saldanha@terceiros.sicredi.com.br>
2020-05-24 17:37:59 -0300

    corrigindo chromedriver CI

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,29 +12,16 @@ Capybara.configure do |config|
   config.default_max_wait_time = 15
     #  page.current_window.maximize
   config.app_host = "https://www.sicredi.com.br/html/ferramenta"
+  config.default_driver = BROWSER = (ENV['DRIVER'] || 'chrome').to_sym
 end
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless no-sandbox) }
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('start-maximized')
+  options.add_argument('disable-infobars')
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options
   )
-
-  if ENV['SELENIUM_DRIVER_URL'].present?
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: ENV.fetch('SELENIUM_DRIVER_URL'),
-      desired_capabilities: capabilities
-    )   
-  else
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome,
-      desired_capabilities: capabilities
-  end 
 end
-
-Capybara.javascript_driver = :headless_chrome
